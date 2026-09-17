b98842054ad473e41cf54f8b4fb7fe31bf4905c8
Sayu447 <danilagam79@gmail.com>
2026-09-17 22:29:02 +0300

    Update header text in app.py to correct automated deployment message

--- app/app.py
+++ app/app.py
@@ -59,7 +59,7 @@ def index():
         <title>Docker Final Project</title>
       </head>
       <body>
-        <h1>Автоматический CD работает!!</h1>
+        <h1>Автоматический деплой работает!</h1>
         <p>Количество посещений: {visits}</p>
         <p>Nginx → Python → PostgreSQL</p>
       </body>
